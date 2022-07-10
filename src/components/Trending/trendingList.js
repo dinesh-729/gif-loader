2fc0426455694b0beb2ca7c22bd6107972e4bd10
Dinesh <dinesh729.dm@gmail.com>
2022-07-10 18:05:46 +0530

    adding carousel arrows

--- src/components/Trending/trendingList.js
+++ src/components/Trending/trendingList.js
@@ -1,16 +1,76 @@
+import { useState, useRef } from "react";
 import TextCard from "./TextCard";
+import Arrow from "../Arrow";
 
 import './trending.css';
 
 const TrendingList = ({trending}) => {
-    const searchUrl = "/search?q="
+    const searchBaseUrl = "/search?q=";
+    const [show, setShow] = useState({prev:false, next:true});
+    const scrollPosition = useRef(0);
+    const scrollWidth = useRef(0);
+
+    const handlerNext = () => {
+        const featured = document?.querySelector(".trending-list");
+        if(!featured) return;
+        const position = featured.getBoundingClientRect();
+
+        if(!scrollWidth.current) {
+            scrollWidth.current = featured.childElementCount * featured.querySelector("a")?.getBoundingClientRect()?.width;
+        }
+
+        scrollPosition.current = scrollPosition.current + position?.width;
+        featured.scrollTo(scrollPosition.current,0);
+        if(scrollWidth.current<=scrollPosition.current) {
+            setShow(prev=>({
+                ...prev,
+                next: false   
+            }))
+        }
+        setShow(prev=>({
+            ...prev,
+            prev: true
+        }))
+    }
+    const handlerPrev = () => {
+        const featured = document?.querySelector(".trending-list");
+        if(!featured) return;
+        const position = featured.getBoundingClientRect();
+
+        scrollPosition.current = scrollPosition.current - position?.width;
+        featured.scrollTo(scrollPosition.current,0);
+        
+        if(featured.scrollLeft===0) {
+            setShow(prev=>({
+                ...prev,
+                prev: false   
+            }))
+        }
+        setShow(prev=>({
+            ...prev,
+            next: true   
+        }))
+    }
+
     return(
-        <div className="trending-list">
-            {trending?.results?.map((text, index)=> (
-                <a key={`trending${index}`} href={searchUrl + encodeURIComponent(text)}>
-                    <TextCard text={text} />
-                </a>
-            ))}
+        <div className="trending-wrapper">    
+            {show.prev && 
+                <div className="prev-arrow" onClick={handlerPrev}>
+                    <Arrow />
+                </div>
+            }
+            <div className="trending-list">
+                {trending?.results?.map((text, index)=> (
+                    <a key={`trending${index}`} href={searchBaseUrl + encodeURIComponent(text)}>
+                        <TextCard text={text} />
+                    </a>
+                ))}   
+            </div>         
+            {show.next && 
+                <div className="next-arrow" onClick={handlerNext}>
+                    <Arrow />
+                </div>
+            }
         </div>
     )
 }
